a693274147c999588e03600aabc9f710baf24947
Eric <ericyoung111@gmail.com>
2021-11-30 20:48:57 -0700

    feature/server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,19 @@
 const router = require('express').Router();
-const { deleteFromMemory } = require('../lib/notes');
-const notes = require('../db/db.json');
+const notesDB = require('../lib/notes');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
+    const results = notesDB.getNotes();
     res.json(results);
 });
 
 router.delete('/notes/:id', (req, res) => {
-    var newNotesArray = deleteFromMemory(req.params.id, notes)
-    notes = newNotesArray
-    res.send('deleting notes')
-//delete out of the notes object
-//delete out of the db.json
+    notesDB.deleteFromMemory(req.params.id)
+    res.status(200).send('note deleted')
 });
 
 router.post('/notes', (req, res) => {
-    res.send('posting notes')
-  // set id based on what the next index of the array will be
-//   req.body.id = animals.length.toString();
-
-//   if (!validateAnimal(req.body)) {
-//     res.status(400).send('The animal is not properly formatted.');
-//   } else {
-//     const animal = createNewAnimal(req.body, animals);
-//     res.json(animal);
-//   }
+    notesDB.writeNewTask(req.body)
+    res.status(200).send('posting notes')
 });
 
 module.exports = router;
